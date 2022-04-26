2812b6c91822e18e54930a2fa2c209e5752b5179
TranQuocBao97 <100355000+TranQuocBao97@users.noreply.github.com>
2022-04-26 16:36:21 +0700

    up

--- Module2/src/ss19_string_adn_regex/bai_tap/validate_ten_lop_hoc/ValidateTenLopHoc.java
+++ Module2/src/ss19_string_adn_regex/bai_tap/validate_ten_lop_hoc/ValidateTenLopHoc.java
@@ -1,16 +1,20 @@
 package ss19_string_adn_regex.bai_tap.validate_ten_lop_hoc;
 
-import javafx.scene.SnapshotParameters;
 
 import java.util.Scanner;
 
 public class ValidateTenLopHoc {
     private static final String NAME_CLASS_REGEX = "^[C,A,P][0-9]{4}[G,H,I,K,L,M]$";
+    private static final String NAME_CLASS_REGEX_2 = "^[C,A,P]\\d{4}[G,H,I,K,L,M]$";
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.print("Nhập tên lớp học: ");
         String className = scanner.nextLine();
         while (!(className.matches(NAME_CLASS_REGEX))){
+            System.out.print("Tên Lớp học ko hợp lệ, xin nhập lại: ");
+            className = scanner.nextLine();
+        }
+        while (!(className.matches(NAME_CLASS_REGEX_2))){
             System.out.print("Tên Lớp học ko hợp lệ, xin nhập lại: ");
             className = scanner.nextLine();
         }
