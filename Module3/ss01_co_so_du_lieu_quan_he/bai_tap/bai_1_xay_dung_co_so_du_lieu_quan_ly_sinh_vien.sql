b64bd76f572c3e48d79d84f672d880bdadaec18d
TranQuocBao97 <100355000+TranQuocBao97@users.noreply.github.com>
2022-05-13 10:20:01 +0700

    up

--- Module3/ss01_co_so_du_lieu_quan_he/bai_tap/bai_1_xay_dung_co_so_du_lieu_quan_ly_sinh_vien.sql
+++ Module3/ss01_co_so_du_lieu_quan_he/bai_tap/bai_1_xay_dung_co_so_du_lieu_quan_ly_sinh_vien.sql
@@ -1,20 +1,23 @@
-CREATE DATABASE student_management;
+DROP DATABASE IF EXISTS student_management;
+CREATE DATABASE IF NOT EXISTS student_management;
+
+USE student_management;
 
 CREATE TABLE student (
 id INT PRIMARY KEY,
-name VARCHAR (45),
+`name` VARCHAR (16335),
 age INT,
 country VARCHAR(45)
 );
 
 CREATE TABLE class (
 id INT PRIMARY KEY,
-name VARCHAR (50)
+`name` VARCHAR (50)
 );
 
 CREATE TABLE teacher (
 id INT PRIMARY KEY,
-name VARCHAR (60),
+`name` VARCHAR (60),
 age INT,
 country VARCHAR(40)
 );
