d5e85f54b9e50427c2b31abd0233d3e6caad3dac
BaoAsus <BaoAsus@gmail.com>
2022-07-27 21:26:19 +0700

    up

--- Module5/caseStudy01/src/app/contract/contract-add/contract-add.component.ts
+++ Module5/caseStudy01/src/app/contract/contract-add/contract-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-contract-add',
@@ -16,12 +17,20 @@ export class ContractAddComponent implements OnInit {
 
   getContractForm(){
     this.contractForm = new FormGroup({
-      startDate: new FormControl("",[Validators.required]),
-      endDate: new FormControl("",[Validators.required]),
+      startDate: new FormControl("",[Validators.required, this.validateDate]),
+      endDate: new FormControl("",[Validators.required, this.validateDate]),
       facility: new FormControl("",[Validators.required]),
       contractOtherServices: new FormControl("",[Validators.required])
     })
   }
+
+  validateDate(AC: AbstractControl) {
+    if (AC && AC.value && !moment(AC.value, 'YYYY-MM-DD', true).isValid()) {
+      return { 'dateValidator': true };
+    }
+    return null;
+  }
+
 }
 
 export class ContractAddAppComponent {
